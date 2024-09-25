bd56b2e8c1023c986764ac8699d2df351a31b29d
Ammar Shabbir <ashabbir197@gmail.com>
2024-09-25 15:20:38 -0400

    added log page

---
+++ src/pages/admin/logs/Logs.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import LogDatagrid from "../../../components/LogDatagrid/LogDatagrid";
+
+export default function Logs() {
+  const [value, setValue] = useState("admin");
+
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Admins" id="simple-tab-0" value="admin" />
+          <Tab
+            label="Corperate Client"
+            id="simple-tab-1"
+            value="corperate-client"
+          />
+          <Tab label="Customer" id="simple-tab-2" value="customer" />
+        </Tabs>
+      </Box>
+
+      <Box
+        sx={{
+          marginTop: 3,
+        }}
+      >
+        <LogDatagrid value={value} />
+      </Box>
+    </Box>
+  );
+}
